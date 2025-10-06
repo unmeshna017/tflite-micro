c90a3d50f2404a91236f6611b32224963208db31
unmesna017 <unmeshna@cadence.com>
2025-10-06 08:29:46 -0700

    Updated xtensa_download.sh script for the latest NNLib packages. Removed HiFi NNLib patch files.

--- tensorflow/lite/micro/tools/make/ext_libs/xtensa_download.sh
+++ tensorflow/lite/micro/tools/make/ext_libs/xtensa_download.sh
@@ -41,13 +41,13 @@ DOWNLOADS_DIR=${1}
 PATCH="../../ext_libs/xa_nnlib_${2}.patch"
 
 if [[ ${2} == "hifi4" ]]; then
-  LIBRARY_URL="http://github.com/foss-xtensa/nnlib-hifi4/raw/master/archive/xa_nnlib_hifi4_02_11_2025.zip"
+  LIBRARY_URL="http://github.com/foss-xtensa/nnlib-hifi4/raw/master/archive/xa_nnlib_hifi4_09_30_2025.zip"
   LIBRARY_DIRNAME="xa_nnlib_hifi4"
-  LIBRARY_MD5="7eed33ff8fac0cb33619031df6f7171e"
+  LIBRARY_MD5="a1cba57501a35d0bffdd39c536689366"
 elif [[ ${2} == "hifi5" ]]; then
-  LIBRARY_URL="http://github.com/foss-xtensa/nnlib-hifi5/raw/master/archive/xa_nnlib_hifi5_02_11_2025.zip"
+  LIBRARY_URL="http://github.com/foss-xtensa/nnlib-hifi5/raw/master/archive/xa_nnlib_hifi5_09_30_2025.zip"
   LIBRARY_DIRNAME="xa_nnlib_hifi5"
-  LIBRARY_MD5="be718765a8d110856d6dbb2ce7fc8b68"
+  LIBRARY_MD5="f3ac445ae47143b1fddf26b85d763697"
 elif [[ ${2} == "vision_p6" ]]; then
   LIBRARY_URL="https://github.com/foss-xtensa/tflmlib_vision/raw/main/archive/xi_tflmlib_vision_p6_22_06_29.zip"
   LIBRARY_DIRNAME="xi_tflmlib_vision_p6"
