b701347cb61eaa0d0728e80b41fe504f472bf491
unmeshna <unmeshna@cadence.com>
2026-07-15 10:40:38 -0700

    Updated output dims data initialization logic when output_size=0

--- tensorflow/lite/micro/kernels/xtensa/reduce_hifi.cc
+++ tensorflow/lite/micro/kernels/xtensa/reduce_hifi.cc
@@ -30,7 +30,7 @@ limitations under the License.
 namespace tflite {
 
 const int kMaxNumberOfAxisHifi = 5;
-const int kMaxNumberOfReducedAxisHifi = 2;
+const int kMaxNumberOfReducedAxisHifi = 4;
 
 extern TfLiteStatus PrepareSimple(TfLiteContext* context, TfLiteNode* node,
                            int32_t* multiplier, int* shift);
@@ -302,12 +302,19 @@ TfLiteStatus EvalMeanHifi(TfLiteContext* context, TfLiteNode* node,
         p_scratch = static_cast<void*>(
         context->GetScratchBuffer(context, xt_data->scratch_tensor_index));
 
+        int output_size = output->dims->size;
+        int output_dims_data[1] = {1};
+        int* out_dims_ptr = output->dims->data;
+        if(output_size == 0){
+          output_size = 1;
+          out_dims_ptr = output_dims_data;
+        }
         err = xa_nn_reduce_mean_4D_asym8s_asym8s(output_data_ptr,
-                                                 output->dims->data,
+                                                 out_dims_ptr,
                                                  input_data_ptr,
                                                  input->dims->data,
                                                  resolved_axis,
-                                                 output->dims->size,
+                                                 output_size,
                                                  input->dims->size,
                                                  num_resolved_axis,
                                                  op_data->input_zp,
